562f796c2b5c295467751c9686412b1840edbbdd
kleberfort <kleberfort@gmail.com>
2022-08-13 12:23:17 -0300

    first project

---
+++ app/src/main/java/com/example/dadosdosjogos2020oficial/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dadosdosjogos2020oficial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.dadosdosjogos2020oficial.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+    }
+}
